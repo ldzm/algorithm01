b06cccccab067c48aef7e955c5ba92b7267f00cd
ldzm <ldzm_zg@126.com>
2017-07-12 09:59:14 +0800

    最短路径算法：Floyd-Warshall 算法代码和测试代码

---
+++ src/main/java/algorithm01/chapter6/FloydWarshall.java
@@ -0,0 +1,20 @@
+package algorithm01.chapter6;
+
+public class FloydWarshall {
+
+	public final static int INF_NUM = Integer.MAX_VALUE;
+
+	public static void floyd(int[][] e, int n) {
+		for (int k = 1; k <= n; k++) {
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					// e[i][k] + e[k][j] < e[i][j] 为了防止超过整数的范围溢出，所以写成
+					// e[i][k] < e[i][j] - e[k][j] 
+					if (e[i][k] < INF_NUM && e[k][j] < INF_NUM && e[i][k] < e[i][j] - e[k][j]) {
+						e[i][j] = e[i][k] + e[k][j];
+					}
+				}
+			}
+		}
+	}
+}
