5e8cd3b98903a288f2e7b735c0c7c78e4fcf8c39
ldzm <ldzm_zg@126.com>
2017-07-12 11:54:44 +0800

    单源最短路径求解：Dijkstra
    算法和测试代码

---
+++ src/main/java/algorithm01/chapter6/Dijkstra.java
@@ -0,0 +1,44 @@
+package algorithm01.chapter6;
+
+public class Dijkstra {
+	public final static int INF_NUM = Integer.MAX_VALUE;
+
+	/**
+	 * 点u到其他点的最短距离
+	 * @param e
+	 * @param u
+	 * @param n
+	 */
+	public static int[] dijkstra(int[][] e, int u, int n) {
+
+		int[] dis = new int[n + 1];
+		int[] book = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
+			dis[i] = e[u][i];
+			book[i] = 0;
+		}
+
+		book[u] = 1;
+		for (int i = 1; i <= n - 1; i++) {
+			int min = INF_NUM;
+			for (int j = 1; j <= n; j++) {
+				if (0 == book[j] && dis[j] < min) {
+					min = dis[j];
+					u = j;
+				}
+			}
+
+			book[u] = 1;
+			for (int k = 1; k <= n; k++) {
+				if (e[u][k] < INF_NUM) {
+					if (dis[k] > dis[u] + e[u][k]) {
+						dis[k] = dis[u] + e[u][k];
+					}
+				}
+			}
+		}
+		
+		return dis;
+	}
+}
---
+++ src/test/java/algorithm01/chapter6/DijkstraTest.java
@@ -0,0 +1,90 @@
+package algorithm01.chapter6;
+
+import java.util.Arrays;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class DijkstraTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public DijkstraTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( DijkstraTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testFloyd()
+    {
+    	
+    	int m = 8;
+    	int n = 4;
+    	int e[][] = new int[n + 1][n + 1];
+    	int a[][] = 
+    		{
+    			{1, 2, 2},
+    			{1, 3, 6},
+    			{1, 4, 4},
+    			{2, 3, 3},
+    			{3, 1, 7},
+    			{3, 4, 1},
+    			{4, 1, 5},
+    			{4, 3, 12}
+    		};
+    	
+    	for (int i = 1; i <= n; i++)
+    	{
+    		for(int j = 1; j <= n; j++)
+    		{
+    			if (i == j)
+    			{
+    				e[i][j] = 0;
+    			}
+    			else
+    			{
+    				e[i][j]  = FloydWarshall.INF_NUM;
+    			}
+    		}
+    	}
+    	
+    	for(int i = 0; i < m; i++)
+    	{
+    		int num1 = a[i][0];
+    		int num2 = a[i][1];
+    		e[num1][num2] = a[i][2];
+    	}
+
+    	
+    	int [][] result = {
+    			{0, 0, 0, 0, 0},
+    			{0, 0, 2, 5, 4},
+    			{0, 9, 0, 3, 4},
+    			{0, 6, 8, 0, 1},
+    			{0, 5, 7, 10, 0},
+    	};
+    	
+    	for(int i = 1; i <= n; i++) {
+    		int[] res = Dijkstra.dijkstra(e, i, n);
+    		assertTrue(Arrays.equals(res, result[i]));
+    	}
+    }
+}
