370ba72bb9167d03270f1a1d3c5e1e99c75c6f8d
ldzm <ldzm_zg@126.com>
2017-08-14 22:28:02 +0800

    测试基础知识的代码

--- src/main/java/algorithm01/test/Test.java
+++ src/main/java/algorithm01/test/Test.java
@@ -21,17 +21,42 @@ public class Test {
 		A a = new B();
 		a.play();
 
+		// final 方法可以被继承，不可以被覆盖
+		B b = new B();
+		b.play();
 	}
 }
 
 class A {
+	
+	private int a;
+	public int b;
 	public final static int A_NUM = 10;
 	
 	final void play() {
 		System.out.println("final play();");
 	}
+	
+	private final void run() {
+		System.out.println("A run");
+	}
 }
 
 class B extends A {
+	private int a;
+	public int b;
+	
 	public final static int A_NUM = 10;
+	
+	private final void run() {
+		System.out.println("B run");
+	}
+	
+	public static void main(String[] args) {
+		new  B().run();
+		
+		new B().b = 1;
+		
+		System.out.println(true|true);
+	}
 }
